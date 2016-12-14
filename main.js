a9f5506467ea152771a5090ed439656f8306b7b5
harvidson <heather.arvidson@gmail.com>
2016-12-14 15:19:54 -0800

    to question 10
    completed exercises up to question 10

--- main.js
+++ main.js
@@ -15,26 +15,26 @@ ADVANCED TRACK: 11-15
 
 
 
-// 1. Create a function that simply returns a string that says something funny 
+// 1. Create a function that simply returns a string that says something funny
 //    when it is called and then displayed into the HTML page.
 
 
 
-// 2. Define a function called "divideByTwo". 
+// 2. Define a function called "divideByTwo".
 //    It should accept one parameter within a text field called "number" and
-//    submitted when a button is clicked. 
-//    The function should divide the number by two and 
+//    submitted when a button is clicked.
+//    The function should divide the number by two and
 //    then displayed into the HTML page.
 
 
 
-// 3. Define a function called "greeting". 
+// 3. Define a function called "greeting".
 //    It should accept two parameters within input fields, which will both be first names.
 //    The function should display to the HTML page a greeting to both people.
 
 
 
-// 4. Create a function that finds the average of 6 numbers passed in when called 
+// 4. Create a function that finds the average of 6 numbers passed in when called
 //    and returns the value and is then displayed in the HTML page.
 
 
@@ -42,46 +42,83 @@ ADVANCED TRACK: 11-15
 
 // 5. You use Amazon Subscribe & Save to have six cans of cashews automatically sent to you each month.
 //    Write a function that takes the price per unit and calculates the total for you each month.
-//    Since the price of the cashews can change anytime, pass that amount into the function to get your total price. 
+//    Since the price of the cashews can change anytime, pass that amount into the function to get your total price.
 
+function total(price, cans){
+  return price * cans;
+}
+
+console. log(total(3.45, 9);
 
 
 
 // 6. Define functions called "area" and "perimeter". They should both accept two parameters and calculate the area and perimeter of a rectangle.
+function area(length, width){
+  return length * width;
+}
 
-
+function perimeter(length, width){
+  return (2 * length) + (2 * width);
+}
+console.log("The rectangle's perimeter is " + perimeter(6, 8) + " meters.");
+console.log("The rectangle's area is " + area(6, 8) + " meters.");
 
 
 
 // 7. Define a function called "quarter". It accepts one parameter called "number".
 //    The function will return a value which is one quarter of the number provided.
+function quarter(number){
+  return number / 4;
+}
 
-
+console.log(quarter(88));
 
 
 
 // 8. Write a function called "sleepings", it should accept one parameter, an integer called "hours".
 //    Write a conditional statement inside of the function. If the number of hours is more than 8, display a statement to the HTML page about getting enough rest.
 //    If the number of hours is less than 8, print a statement recommending the user get more shut eye.
-
-
+function sleepings(hours){
+  if (hours > 8) {
+    return "Well done! You're getting enough sleep!";
+  }
+  else {
+    return "Sleepy? Either way, you should try to get some more shuteye.";
+  }
+}
+console.log(sleepings(2.3));
 
 
 
 // 9. Develop a function that determines a person's age by asking them for their birth year.
+var year = 2016
+function age(birthYear){
+  return (year - birthYear);
+}
+
+console.log("you are " + age(1987) + " years old.");
+console.log("you are " + age(1979) + " years old.");
 
 
-
-
-
-// 10. Write a function that accepts the following array and separates the people into two teams. Display each team within the HTML page. 
+// 10. Write a function that accepts the following array and separates the people into two teams. Display each team within the HTML page.
 //    No names next to each other in the array should be on the same team.
-     teammates = ["Harold", "Bob", "Sue", "Grady", "Notorious B.I.G.", "JavaCrypt", "Weird guy in the back", "Gary", "Carol", "Hipster Tim", "Janet"]
 
+teammates = ["Harold", "Bob", "Sue", "Grady", "Notorious B.I.G.", "JavaCrypt", "Weird guy in the back", "Gary", "Carol", "Hipster Tim", "Janet"]
+var team1 = [];
+var team2 = [];
 
+function teamDivider(arrayOfPeople){
+for (i = 0; i < arrayOfPeople.length; i+= 2){
+ team1.push(arrayOfPeople[i]);
+}
+for (i = 1; i < arrayOfPeople.length; i+= 2){
+ team2.push(arrayOfPeople[i]);
+}
+console.log("Team 1 members are " + team1);
+}
+teamDivider(teammates);
 
-
-// 11. Allow a user to enter a three digit number. 
+// 11. Allow a user to enter a three digit number.
 //     Write a function that adds the numbers together.
 //     Hint #1: You need to turn a string into an integer.
 //     Hint #2: Strings can be treated as arrays too.
@@ -90,7 +127,7 @@ ADVANCED TRACK: 11-15
 
 
 
-// 12. You've finally gotten around to counting the change in your piggy bank. 
+// 12. You've finally gotten around to counting the change in your piggy bank.
 //     Write a function that accepts four parameters from a user (quarters, dimes, nickels, and pennies).
 //     The function should take each number of coins and multiply it times each coin's value.
 //     Finally, it should return the total amount of change you have in the following format: "$32.77"
@@ -131,7 +168,7 @@ ADVANCED TRACK: 11-15
 
 // C. Create a die rolling function that accepts no parameters.
 //     It rolls two six-sided-dice, adds the two numbers together, and returns a roll value.
-//     Display the result in the HTML page.   
+//     Display the result in the HTML page.
 //     To get the random number rolled by each die, use Math.random and Math.floor.
 
 
@@ -141,7 +178,7 @@ ADVANCED TRACK: 11-15
 
 
 
-// E. Write a function that takes a year from a user 
+// E. Write a function that takes a year from a user
 //    and reports whether or not it is a leap year.
 //    Display the result in the HTML page.
 //    Remember, a leap year occurs:
@@ -149,11 +186,3 @@ ADVANCED TRACK: 11-15
 //        Except every year that is evenly divisible by 100
 //        Unless the year is also evenly divisible by 400
 //        For example, 1997 is not a leap year, but 1996 is. 1900 is not a leapyear, but 2000 is.
-
-
-
-
-
-
-
-
